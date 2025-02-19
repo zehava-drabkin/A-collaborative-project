8603d0d667230de87458126c29255d53bfe052cb
zehava-drabkin <rdrbig@gmail.com>
2025-02-20 01:11:32 +0200

    Turning an execute function into a global function

--- Ffmpeg.Service/Commands/WatermarkCommand.cs
+++ Ffmpeg.Service/Commands/WatermarkCommand.cs
@@ -22,33 +22,10 @@ namespace Ffmpeg.Command.Commands
                 .SetOutput(outputFilePath)
                 .Build();
 
-            var result = await RunFFmpegCommand(arguments);
+            var result = await FfmpegExecutor.RunFFmpegCommand(arguments);
             return result
                 ? new Result { IsSuccess = true, OutputPath = outputFilePath }
                 : new Result { IsSuccess = false, ErrorMessage = "Failed to add watermark" };
-        }
-
-        private async Task<bool> RunFFmpegCommand(string arguments)
-        {
-            //צריך לעוף מפה
-            var ffmpegPath = @"C:\Program Files\ffmpeg-2025-02-17-git-b92577405b-full_build\bin\ffmpeg.exe";
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = ffmpegPath,
-                    Arguments = arguments,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                }
-            };
-
-            process.Start();
-            await process.WaitForExitAsync();
-
-            return process.ExitCode == 0;
         }
     }
 }
---
+++ Ffmpeg.Service/FfmpegExecutor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ffmpeg.Command
+{
+    public static class FfmpegExecutor
+    {
+        public static async Task<bool> RunFFmpegCommand(string arguments)
+        {
+            //צריך לעוף מפה
+            var ffmpegPath = @"C:\Program Files\ffmpeg-2025-02-17-git-b92577405b-full_build\bin\ffmpeg.exe";
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = ffmpegPath,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            await process.WaitForExitAsync();
+
+            return process.ExitCode == 0;
+        }
+
+    }
+}
